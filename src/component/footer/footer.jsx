0619e616bb1f1a1af9b5218e97bc89ba353c0aea
KwinBee-Admin <146941464+KwinBee-Admin@users.noreply.github.com>
2024-11-08 10:48:12 +0530

    Update footer.jsx

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -19,8 +19,13 @@ const Footer = () => {
             </a>
           </li>
           <li>
+            <a href="https://wa.link/weua1t" target="_blank" rel="noopener noreferrer">
+              <img src={whatsapp} alt="WhatsApp" /> +44 77860 33126
+            </a>
+          </li>
+          <li>
             <a href="mailto:chess@kwinbee.com" target="_blank" rel="noopener noreferrer">
-              <img src={email} alt="Email" /> chess@kwinbee.com
+              <img src={email} alt="Email" /> onlychess64@gmail.com
             </a>
           </li>
         </ul>
